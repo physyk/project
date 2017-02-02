927c3f5662ed2a84d3f666ce5c148d5f379a1801
Hellon Canella <helloncanella@gmail.com>
2017-02-02 19:54:55 -0200

    added graphics

---
+++ basic-library.js
@@ -0,0 +1,75 @@
+function degToRad(deg) {
+    return Math.PI / 180 * deg
+}
+
+function cos(deg) {
+    return Math.cos(degToRad(deg))
+}
+
+function sin(deg) {
+    return Math.sin(degToRad(deg))
+}
+
+function clone(object) {
+    return Object.assign({}, object)
+}
+
+
+function verlet(r0, v0, aceleration, deltaT) {
+    let r, v, a = aceleration(r0)
+
+    r = r0 + v0 * deltaT + a / 2 * Math.pow(deltaT, 2)
+    v = v0 + (aceleration(r,v) + a) / 2 * deltaT
+
+    return { r, v }
+}
+
+
+function euler(r0, v0, aceleration, deltaT) {
+    let r, v
+
+    r = r0 + v0 * deltaT
+    v = v0 + aceleration(r,v) * deltaT
+
+    return { r, v }
+}
+
+
+function simulation(simulationMethod, importInfo) {
+
+    const {iterations, timeStep, initialConditions, acelerationX, acelerationY} = importInfo
+
+    let lastSimulationData = clone(initialConditions)
+        , simulationData = { x: [], y: [] }
+
+    for (let i = 1; i <= iterations; i++) {
+
+        const
+            {r: rx, v: vx} = simulationMethod(lastSimulationData.r.x, lastSimulationData.v.x, acelerationX, timeStep)
+            , {r: ry, v: vy} = simulationMethod(lastSimulationData.r.y, lastSimulationData.v.y, acelerationY, timeStep)
+            , iterationData = {
+                r: { x: rx, y: ry },
+                v: { x: vx, y: vy }
+            }
+
+        simulationData.x.push(iterationData.r.x)
+        simulationData.y.push(iterationData.r.y)
+
+        lastSimulationData = clone(iterationData)
+
+    }
+
+    return simulationData
+
+
+}
+
+
+function numericalSolution(simulationInitialInfo){
+
+    const eulerData = simulation(euler, simulationInitialInfo)
+        , verletData = simulation(verlet, simulationInitialInfo)
+
+    return { eulerData, verletData }
+
+}
---
+++ comparison-euler-verlet.js
@@ -0,0 +1,94 @@
+function acelerationX(x) {
+    return 0
+}
+
+function acelerationY(y) {
+    return -9.81
+}
+
+function realSolution(simulationInfo){
+
+    const {iterations, initialConditions, acelerationY, timeStep} = simulationInfo
+
+    const {r, v} = initialConditions
+        , {x: x0, y: y0} = r
+        , {x: vx0, y: vy0} = v
+    
+    let result = {x:[], y:[]}
+
+    for (let i = 1; i <= iterations; i++) {
+
+        const t = timeStep * i
+
+        let x, y
+
+        x = x0 + vx0 * t
+        y = y0 + vy0 * t + acelerationY() / 2 * Math.pow(t, 2)
+
+        result.x.push(x)
+        result.y.push(y)
+    }
+
+
+    return result
+}
+
+
+
+const initialConditions = {
+    r: {
+        x: 0,
+        y: 0
+    },
+    v: {
+        x: 10 * cos(30),
+        y: 10 * sin(30)
+    }
+}
+
+
+
+const simulationInfo = {
+    iterations: 100,
+    timeStep: 0.01,
+    initialConditions,
+    acelerationX,
+    acelerationY
+}
+
+const {eulerData, verletData, realDataArray} = numericalSolution(simulationInfo)
+      , realSolutionData = realSolution(simulationInfo)  
+
+var trace2 = {
+    x: eulerData.x,
+    y: eulerData.y,
+    mode: 'markers',
+    name: 'Euler'
+};
+
+var trace3 = {
+    x: verletData.x,
+    y: verletData.y,
+    mode: 'markers',
+    name: 'Verlet'
+};
+
+var trace4 = {
+    x: realSolutionData.x,
+    y: realSolutionData.y,
+    mode: 'markers',
+    name: 'Real Solution'
+};
+
+var data = [trace2, trace3, trace4];
+
+var layout = {
+    title: 'Comparação Verlet Vs Euler para um lançamento de projétil de 30m/s ',
+    height: 700,
+    width: 1000
+};
+
+Plotly.newPlot('myDiv', data, layout);
+
+
+
